8bb690536696b96026c04daf5d42f98576c76f43
Wesley Oliveira <32249497+Wesley-Oliveira@users.noreply.github.com>
2021-04-13 15:28:03 -0300

    Work with materials with Unity

---
+++ Assets/_Scripts/ColorChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public Color targetColor;
+    public Texture targetTex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Material material = renderer.material;
+        material.color = targetColor;
+        //efeito global renderer.sharedMaterial.color = targetColor;
+        if(material.HasProperty("_MainTex"))
+        {
+            material.SetTexture("_MainTex", targetTex);
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Materials-Unity-3D
 Project created to explore the materials topics with Unity.
+
+
+## Unity Version
+The project was developed using Unity 2019.4.21f1
